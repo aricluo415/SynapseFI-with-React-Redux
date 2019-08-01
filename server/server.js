a2bbc6cf3e7b6d087298856a1cb9c91c9b68bca8
Aric Luo <aricluo@Arics-MacBook-Pro.local>
2019-07-31 18:42:35 -0700

    fixed spend

--- server/server.js
+++ server/server.js
@@ -7,10 +7,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Sucessfully Connected to Server
-app.get("/api/server", (req, res) => {
-  res.send({ express: "Successfully connected to server." });
-});
 app.get("/api/users", (req, res) => {
   synapse.getUser().then(function(result) {
     res.json(result.body.legal_names[0]);
