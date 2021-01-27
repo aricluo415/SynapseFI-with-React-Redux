0aca1db5d7e7a167cb8ca70ac87397c207cd4d1d
Aric Luo <aricluo415@gmail.com>
2021-01-27 02:26:08 -0800

    A

--- server/server.js
+++ server/server.js
@@ -8,15 +8,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/api/users", (req, res) => {
-  synapse.getUser().then(function(result) {
+  synapse.getUser().then(function (result) {
     res.json(result.body.legal_names[0]);
   });
 });
 app.post("/api/messages", (req, res) => {
-  synapse.getMessage(req.body.question).then(function(result) {
+  synapse.getMessage(req.body.question).then(function (result) {
     res.json(result);
   });
 });
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
